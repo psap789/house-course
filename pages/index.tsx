712aae0868d6b713c50745801eef9f96ef8502e4
psap789 <145521245+psap789@users.noreply.github.com>
2023-10-04 15:04:36 -0400

    mutation part

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 // import { useState } from "react";
-// import { useQuery, gql } from "@apollo/client";
+// uncomment the below line for HELLO_QUERY
+// import { useQuery, gql } from "@apollo/client"; 
 // import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
 import Map from "src/components/map";
@@ -8,12 +9,23 @@ import Map from "src/components/map";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
 
+// const HELLO_QUERY = gql`
+//   query HelloQuery {
+//     hello
+//   }
+// `;
+
 export default function Home() {
+  // const { data } = useQuery(HELLO_QUERY);
+
   return (
     <Layout
       main={
         <div className="flex">
-          <div className="w-1/2 pb-4" style={{ maxHeight: "calc(100vh - 64px)", overflowX: "scroll" }}>HouseList</div>
+          <div className="w-1/2 pb-4" style={{ maxHeight: "calc(100vh - 64px)", overflowX: "scroll" }}>
+            HouseList
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+          </div>
           <div className="w-1/2">
             < Map />
           </div>
--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
-// import { useMutation, gql } from "@apollo/client";
+import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
 import Link from "next/link";
 // import { Image } from "cloudinary-react";
@@ -13,9 +13,41 @@ import { SearchBox } from "./searchBox";
 //   UpdateHouseMutation,
 //   UpdateHouseMutationVariables,
 // } from "src/generated/UpdateHouseMutation";
-// import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
+import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
 
-export { };
+
+const SIGNATURE_MUTATION = gql`
+    mutation CreateSignatureMutation {
+        createImageSignature {
+            signature
+            timestamp
+        }
+    }
+
+`;
+
+interface IUploadImageResponse {
+    secure_url: string;
+
+}
+
+async function uploadImage(image: File, signature: string, timestamp: number):
+    Promise<IUploadImageResponse> {
+    const url = `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`;
+    const formData = new FormData();
+    formData.append("file", image)
+    formData.append("signature", signature);
+    formData.append("timestamp", timestamp.toString());
+    formData.append("api_key", process.env.NEXT_PUBLIC_CLOUDINARY_KEY ?? "");
+
+    const response = await fetch(url, {
+        method: "post",
+        body: formData
+    });
+    return response.json();
+}
+
+// export { };
 
 interface IFormData {
     address: string;
@@ -32,6 +64,9 @@ export default function HouseForm({ }: IProps) {
     const [previewImage, setPreviewImage] = useState<string>();
     const { register, handleSubmit, setValue, errors, watch } = useForm<IFormData>({ defaultValues: {} });
     const address = watch("address");
+    const [createSignature] = useMutation<CreateSignatureMutation>(
+        SIGNATURE_MUTATION
+    );
 
 
     useEffect(() => {
@@ -41,7 +76,17 @@ export default function HouseForm({ }: IProps) {
     }, [register])
 
     const handleCreate = async (data: IFormData) => {
-        console.log({ data });
+        // console.log({ data });
+        const { data: signatureData } = await createSignature();
+        if (signatureData) {
+            // console.log(signatureData);
+            const { signature, timestamp } = signatureData.createImageSignature;
+            const imageData = await uploadImage(data.image[0], signature, timestamp);
+            // console.log(imageData);
+            // const imageUrl =image
+
+        }
+
     };
 
     const onSubmit = (data: IFormData) => {
